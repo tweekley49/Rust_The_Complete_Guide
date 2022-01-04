2466075589b2e67b7a98dd6f506bec0f7fbe483b
Tyler Weekley <mtweekley@csustudent.net>
2022-01-03 21:12:28 -0500

    Variables and mutability

---
+++ Section_2/variables/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut x = 5;
+
+    println!("The value of x is {}", x);
+
+    x = 6;
+    println!("The value of x is {}", x);
+
+    const SECONDS: i8 = 60;
+    println!("The value of seconds is {}", SECONDS);
+}
