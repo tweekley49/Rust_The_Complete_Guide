73ed4e42b34f5496720f9bc12dc63f81eeb07cb3
Tyler Weekley <mtweekley@csustudent.net>
2022-01-05 21:47:13 -0500

    Finish section 2

---
+++ Section_2/control_flow/src/main.rs
@@ -0,0 +1,47 @@
+fn main(){
+    // let one = 1;
+
+    // if one > 10 {
+    //     println!("True");
+    // } else if one == 1 {
+    //     println!("Equal");
+    // }
+    // else {
+    //     println!("False");
+    // }
+
+    // let mut num = 0;
+    // 'counter: loop {
+    //     println!("Count: {}", num);
+    //     let mut decrease = 5;
+    //     loop {
+    //         println!("Decreasing: {}", decrease);
+    //         if decrease == 4 {
+    //             break;
+    //         }
+    //         if num == 2 {
+    //             break 'counter;
+    //         }
+    //         decrease -= 1; //decrease = decrease - 1
+    //     }
+    //     num += 1; // num = num + 1
+    // }
+
+    // let mut num = 0;
+    // while num <= 5 {
+    //     println!("Num: {}", num);
+    //     num += 1;
+    // }
+
+    // let vec: Vec<i8> = (0..10).collect();
+
+    // for element in vec {
+    //     println!("{}", element);
+    // }
+
+    for number in (1..4).rev() {
+        println!("{}", number)
+    }
+    println!("LIFTOFF!!!!");
+
+}
