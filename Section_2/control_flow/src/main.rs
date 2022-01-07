4dfb29b93a1b62035db17c71e391b19714231776
Tyler Weekley <mtweekley@csustudent.net>
2022-01-07 14:33:57 -0500

    update

--- Section_2/control_flow/src/main.rs
+++ Section_2/control_flow/src/main.rs
@@ -39,9 +39,9 @@ fn main(){
     //     println!("{}", element);
     // }
 
-    for number in (1..4).rev() {
-        println!("{}", number)
-    }
-    println!("LIFTOFF!!!!");
+    // for number in (1..4).rev() {
+    //     println!("{}", number)
+    // }
+    // println!("LIFTOFF!!!!");
 
 }
